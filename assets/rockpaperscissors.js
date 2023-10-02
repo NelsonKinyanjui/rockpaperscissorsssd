25ea3af277a000c0e900761e45d14820a7084b17
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2023-10-02 23:05:08 +0300

    add onclick attribute to the function and  call the function on each

--- assets/rockpaperscissors.js
+++ assets/rockpaperscissors.js
@@ -1,9 +1,11 @@
-let randomNumber = Math.random();
+    function rockGame (){let randomNumber = Math.random();
 
-if (randomNumber >= 0 && randomNumber < 1/3){
-    console.log('Rock');
-}
-else if (randomNumber >= 1/3 && randomNumber < 2/3){
-    console.log('Paper');
-}
-else {console.log('Rock')}
+        if (randomNumber >= 0 && randomNumber < 1/3){
+                console.log('Rock');
+            }
+         else if (randomNumber >= 1/3 && randomNumber < 2/3){
+                console.log('Paper');
+        }
+        else if (randomNumber >= 2/3 && randomNumber < 1)
+    {console.log('Rock')}
+    }
